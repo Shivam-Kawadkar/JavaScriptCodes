50b57f227563ea2bbe9ab0fa4a6a2717c7afaf3f
Shivam <skawadkar07@gmail.com>
2025-11-25 16:50:11 +0530

    Refactor script.js to include function definitions for sum and multiplication, and remove commented-out code

--- Functions/script.js
+++ Functions/script.js
@@ -1,4 +1,36 @@
-// console.log("Hello, World!");
-// "abc".toUpperCase();
-// "xyz".toLocaleLowerCase();
-// [1, 2, 3].push(4);
+// function myfunction()
+// {
+//     console.log("Hii! My name is Shivam Kawadkar.");
+//     console.log("I am learning Full Stack Web Development.");
+//     console.log("I love to code and create new projects.");
+// }
+// myfunction();
+// console.log("");
+// myfunction();
+
+
+// function argument(msg)
+// {
+//     console.log(msg);
+// }
+// argument("Hello! This is a message passed as an argument.");
+
+
+
+// To crate a function to calculate the sum of two numbers
+
+function sum(a, b)
+{
+    console.log("Sum of a and b is ",a + b);
+}
+sum(5, 10);
+sum(20, 30);
+sum(100, 200);  
+
+function multi(x,y)
+{
+    return x * y;
+}
+const result = multi(5, 4);
+console.log("Multiplication of x and y is ", result);
+
