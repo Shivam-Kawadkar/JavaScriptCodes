2032d7233eac3d40f0d6b9df3fc7e1d09825cd76
Shivam <skawadkar07@gmail.com>
2025-11-26 20:51:26 +0530

    Enhance DOM demo: Add new boxes and update headings; style adjustments for better layout

--- DOM/script.js
+++ DOM/script.js
@@ -1,3 +1,10 @@
-console.log("Hello");
-alert("Are You MAd Bro?");
+let h2= document.querySelector('h2');
+h2.innerText=h2.innerText + " OR banayega new  new Websites ";
 
+let divs = document.querySelectorAll('.box');
+let idx = 1;
+for(div of divs)
+{
+    div.innerText = `Shivam Kawadar ${idx}`;
+    idx++;
+}
